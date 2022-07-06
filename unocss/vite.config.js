ba4d6031455bf75ceaad9867d338486c98df8622
zhaodacheng5 <zhaodacheng5@hikvision.com.cn>
2022-07-06 20:23:37 +0800

    feat: unocss

--- unocss/vite.config.js
+++ unocss/vite.config.js
@@ -1,9 +1,16 @@
 import Unocss from "unocss/vite";
+import { presetAttributify, presetUno } from "unocss";
 
 export default {
   plugins: [
     Unocss({
-      /* options */
+      presets: [
+        presetAttributify({
+          /* preset options */
+        }),
+        presetUno(),
+        // ...custom presets
+      ],
     }),
   ],
 };
